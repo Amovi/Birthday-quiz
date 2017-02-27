cea4f66ddc07e9d599d86887ee39c3400667e95d
Amovi <anthony.movizzo@outlook.com>
2017-02-27 14:48:20 -0500

    Updated from Brython Server: 2/27/2017 2:48:19 PM

--- birthday.py
+++ birthday.py
@@ -1,44 +1,12 @@
-"""
-birthday.py
-Author: <your name here>
-Credit: <list sources used, if any>
-Assignment:
-
-Your program will ask the user the following questions, in this order:
-
-1. Their name.
-2. The name of the month they were born in (e.g. "September").
-3. The year they were born in (e.g. "1962").
-4. The day they were born on (e.g. "11").
-
-If the user's birthday fell on October 31, then respond with:
-
-  You were born on Halloween!
-
-If the user's birthday fell on today's date, then respond with:
-
-  Happy birthday!
-
-Otherwise respond with a statement like this:
-
-  Peter, you are a winter baby of the nineties.
-
-Example Session
-
-  Hello, what is your name? Eric
-  Hi Eric, what was the name of the month you were born in? September
-  And what year were you born in, Eric? 1972
-  And the day? 11
-  Eric, you are a fall baby of the stone age.
-"""
 from datetime import datetime
 from calendar import month_name
 name = input ("what is your name?")
 month_b = input ("what is the month you were born in?")
 year = input ("what year were you born in?")
 day = input ("on what day were you born?")
+"get months to line up with numbers somehow idk"
+month = month_name[todaymonth]
 winter = [1,2,3]
 spring = [4,5,6]
 summer = [7,8,9]
 fall =  [10,11,12]
-month = month_name[todaymonth]
