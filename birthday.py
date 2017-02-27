53fac23da05de4ff72553b5c71e49b126559955b
Amovi <anthony.movizzo@outlook.com>
2017-02-27 14:39:13 -0500

    Updated from Brython Server: 2/27/2017 2:39:13 PM

--- birthday.py
+++ birthday.py
@@ -34,6 +34,11 @@ Example Session
 from datetime import datetime
 from calender import month_name
 name = input ("what is your name?")
-month = input ("what is the month you were born in?")
+month_b = input ("what is the month you were born in?")
 year = input ("what year were you born in?")
 day = input ("on what day were you born?")
+winter = [1,2,3]
+spring = [4,5,6]
+summer = [7,8,9]
+fall =  [10,11,12]
+month = month_name[todaymonth]
