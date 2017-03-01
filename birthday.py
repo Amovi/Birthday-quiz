e83b70e6cf96b5267c933331dac675cc23d132c8
Amovi <anthony.movizzo@outlook.com>
2017-03-01 13:57:51 -0500

    Updated from Brython Server: 3/1/2017 1:57:51 PM

--- birthday.py
+++ birthday.py
@@ -5,7 +5,8 @@ month_b = input ("what is the month you were born in?")
 year = input ("what year were you born in?")
 day = input ("on what day were you born?")
 if year == [2017,2016,2015,2014,2013,2012,2011,2010,]
-if year == [2009, 2008,2007,2006,2005,2004,2003,2002]
+if year == [2009, 2008,2007,2006,2005,2004,2003,2002,2001,2000]
+if year == [1999, 1998, 1997
 if month == "january":
     season=winter
 if month == "february":
