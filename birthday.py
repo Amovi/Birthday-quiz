319df6314f069c3d75141ac272b9f3ec6ceae5f2
Amovi <anthony.movizzo@outlook.com>
2017-03-02 14:19:59 -0500

    Updated from Brython Server: 3/2/2017 2:19:59 PM

--- birthday.py
+++ birthday.py
@@ -4,9 +4,12 @@ name = input ("what is your name?")
 month_b = input ("what is the month you were born in?")
 year = input ("what year were you born in?")
 day = input ("on what day were you born?")
-if year == [2017,2016,2015,2014,2013,2012,2011,2010,]
-if year == [2009, 2008,2007,2006,2005,2004,2003,2002,2001,2000]
-if year == [1999, 1998, 1997
+if year == [2017,2016,2015,2014,2013,2012,2011,2010,2009,2008,2007,2006,2005,2004,2003,2002,2001,2000]:
+    yeargroup=two thousands
+if year == [1999, 1998, 1997,1996,1995,1994,1993,1992,1991,1990]:
+    yeargroup=nineties
+if year == [1989,1989,1987,1986,1985,1984,1983,1982,1981,1980]:
+    yeargroup=eighties
 if month == "january":
     season=winter
 if month == "february":
@@ -31,4 +34,4 @@ if month == "november":
     season=fall
 if month == "december":
     season=winter
-print (name "you are a" season "baby born in" yeargroup)
+print (name "you are a" yeargroup "baby born in" season)
